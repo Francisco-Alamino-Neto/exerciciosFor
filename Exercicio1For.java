5f6f32227f4a273244d8c3a3bea6c365d1e5f179
Francisco-Alamino-Neto <franciscoalaminoneto26@gmail.com>
2025-06-02 09:28:07 -0300

    rename exercicio1For.java to Exercicio1For.java

--- Exercicio1For.java
+++ Exercicio1For.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class exercicio1For {
+public class Exercicio1For {
     
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
